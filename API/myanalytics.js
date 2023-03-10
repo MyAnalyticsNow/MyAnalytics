eba5de89ffaadc14e5974b570a4e8557f5f11247
Elijah <46640763+ghwosty@users.noreply.github.com>
2023-03-10 23:58:29 +0000

    a

---
+++ API/myanalytics.js
@@ -0,0 +1,47 @@
+/*
+Elijah Bantugan
+2023
+MyAnalytics
+*/
+
+function l(l) {
+    console.log("%c[MyAnalytics]", "color: purple", l)
+}
+
+const authentication = "a";
+
+(async function () {
+    await fetch("/api/add-view", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            authentication: "myauth",
+            host: window.location.host,
+            location: window.location
+        })
+    })
+})();
+
+window.addEventListener("beforeunload", async (e) => {
+    await fetch("/api/leave-site", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            authentication: "myauth",
+            host: window.location.host,
+            location: window.location
+        })
+    })
+})
+
+
+l("Attempting to execute callback...")
+try {
+    onloadCallback();
+} catch (err) {
+    l("Callback failed...")
+}
