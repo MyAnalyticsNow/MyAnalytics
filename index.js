7b77bcc48e53474b7b34e92c5b58a10bf857f9f7
Elijah <46640763+ghwosty@users.noreply.github.com>
2023-03-10 20:38:36 +0000

    Add stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path")
+const app = express();
+
+function rp(p) {
+    return path.join(__dirname, p);
+}
+
+app.use(express.json())
+app.use(express.static("static"))
+
+app.get("/", (req, res) => {
+    res.sendFile(rp("html/index.html"))
+})
+
+const port = process.env.port;
+app.listen(port, () => {
+    console.log("\x1b[33mServer Running!")
+    console.log("\x1b[31mThis is a development server.\n")
+    console.log(`\x1b[33mRunning on:\x1b[0m\nhttp://localhost${port == 1024 ? ":1024" : ""}`)
+})
